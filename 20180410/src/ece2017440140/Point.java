eb0cd95a3e986ef418b08398c153e48c9c8aa854
anything(2017440140) <kstew16@naver.com>
2018-04-10 16:22:41 +0900

    으아아ㅏ 되어라

---
+++ 20180410/src/ece2017440140/Point.java
@@ -0,0 +1,56 @@
+package ece2017440140;
+
+public class Point {
+	private double x;
+	private double y;
+	
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setX(double xx) {
+		this.x = xx;
+		xx = 100;
+	}
+	
+    public void setY(double yy) {
+		this.y = yy;
+	}
+    
+    public void print() {
+		System.out.println("("+this.getX()+","+this.getY()+")");
+		System.out.println("("+getX()+","+getY()+")");
+		System.out.println("("+this.x+","+this.y+")");
+    }
+	
+	public double distance() {
+		//반환할 결과값 선언
+		double result; //지역변수의 유효범위는 메소드 내부
+		
+		//거리 계산
+		result = Math.sqrt(x*x+y*y);
+		
+		//거리 반환
+		return result;
+	}
+	
+	public Point move(Point p) { //매개 변수 p의 유효범위는 메소드 내부
+	
+		//temp 변수 선언
+		double a;
+	    double b;
+	    //포인트 p 만큼 이동
+		a = this.x + p.x; //x는 필드 변수 라고 this를 사용해서 강조 the filed of this object x
+		b = this.y + p.y;	
+		//새로운 포인트 생성
+		Point pnt = new Point();
+		pnt.x = a;
+		pnt.y = b;	
+		//새로운 포인트 반환
+		return pnt;
+	}
+}
---
+++ 20180410/src/ece2017440140/ece2017440140.java
@@ -0,0 +1,16 @@
+package ece2017440140;
+
+public class ece2017440140 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//==은 주소값이 같은것까지 봄
+		// .equals를 사용해야 내용이 같은 지 알 수 있음.
+		Point pnt = new Point();
+		pnt.setX(1.0); pnt.setY(1.0);
+		pnt.print();
+		
+	}
+	
+
+}
