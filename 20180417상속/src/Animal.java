cfe89ad8a6e018c8c19c79b5aa6abee5f593eea9
2017440114 <jiseob98@gmail.com>
2018-04-17 17:30:39 +0900

    변경사항

---
+++ 20180417상속/src/Animal.java
@@ -0,0 +1,25 @@
+
+public class Animal {
+	//필드
+	public String name;
+	//생성자
+	public Animal(String name) {
+		this.name = name;
+	}
+	//메소드
+    public void sing() {
+		System.out.println(name + " Sing");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+    
+    }
+	
+
---
+++ 20180417상속/src/TestAnimal.java
@@ -0,0 +1,15 @@
+
+public class TestAnimal {
+
+	public static void main(String[] args) {
+		Animal animal = new Animal("µ¿¹°");
+		animal.sing();
+		
+		Dog dog = new Dog("Tina");
+		dog.sing();
+		
+		Cat cat = new Cat("200");
+		cat.sing();
+	}
+
+}
