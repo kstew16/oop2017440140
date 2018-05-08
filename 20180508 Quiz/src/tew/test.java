3f60d7980be80007b976297b019d59f210a4bbbf
anything(2017440140) <kstew16@naver.com>
2018-05-08 17:40:40 +0900

    20180508 수업 끝

---
+++ 20180508 Quiz/src/tew/test.java
@@ -0,0 +1,15 @@
+package tew;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Car myCar = new Car();
+			Bicycle myBicycle = new Bicycle();
+			myBicycle.brake();
+			myBicycle.acc();
+			myCar.brake();
+			myCar.acc();
+	}
+
+}
---
+++ 20180508/src/tew/Playable.java
@@ -0,0 +1,5 @@
+package tew;
+
+public abstract interface Playable {
+	abstract void walk();
+}
