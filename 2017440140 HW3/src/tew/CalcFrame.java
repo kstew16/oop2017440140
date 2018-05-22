f946ea138fa56ca28ee1093e71e0b40b0f8d45ab
anything(2017440140) <kstew16@naver.com>
2018-05-22 19:05:39 +0900

    Calc작업중

--- 2017440140 HW3/src/tew/CalcFrame.java
+++ 2017440140 HW3/src/tew/CalcFrame.java
@@ -9,36 +9,87 @@
 
 	public class CalcFrame extends JFrame implements ActionListener {
 		
-		JButton button; //모든 메소드에서 접근 가능
-		JButton button2;
-		JTextField tf;
-		int a=0;
+		JButton button1,button2,button3,button4,button5,button6,button7,button8,button9,button0; //모든 메소드에서 접근 가능
+		JButton plus,c,equal;
+		JTextField numberField;
+		int result=0;
 		
+		public String tostring() {
+			return("="+result);
+			
+		}
 		CalcFrame() {
 			
-			JLabel label = new JLabel("우-화");
-			this.tf = new JTextField(25);
+			JLabel label = new JLabel("Calculator(Plus only)");
+			this.numberField = new JTextField(25);
 
-			tf.setText("여기에 위시를 입력하세요");
+			numberField.setText("0");
 			
 			this.setLayout(new FlowLayout());//플로우 배치 매니저 사용
-			this.button = new JButton("FaButton");
-			button.setActionCommand("button1");
-			this.button2 = new JButton("Able");
+			this.button1 = new JButton("1");
+			button1.setActionCommand("button1");
+			this.button2 = new JButton("2");
 			button2.setActionCommand("button2");
+			this.button3 = new JButton("3");
+			button3.setActionCommand("button3");
+			this.button4 = new JButton("4");
+			button4.setActionCommand("button4");
+			this.button5 = new JButton("5");
+			button5.setActionCommand("button5");
+			this.button6 = new JButton("6");
+			button6.setActionCommand("button6");
+			this.button7 = new JButton("7");
+			button7.setActionCommand("button7");
+			this.button8 = new JButton("8");
+			button8.setActionCommand("button8");
+			this.button9 = new JButton("9");
+			button9.setActionCommand("button9");
+			this.button0 = new JButton("0");
+			button0.setActionCommand("button0");
+			this.plus = new JButton("+");
+			plus.setActionCommand("plus");
+			this.c = new JButton("C");
+			c.setActionCommand("c");
+			this.equal = new JButton("=");
+			equal.setActionCommand("equal");
 			
-			button.addActionListener(this);
+			button1.addActionListener(this);
 			button2.addActionListener(this);
+			button3.addActionListener(this);
+			button4.addActionListener(this);
+			button5.addActionListener(this);
+			button6.addActionListener(this);
+			button7.addActionListener(this);
+			button8.addActionListener(this);
+			button9.addActionListener(this);
+			button0.addActionListener(this);
+			
+			
 			
 			add(label);
-			add(tf);
-			add(button); //여기 배치한게 순서
+			add(numberField);
+			add(button1); //여기 배치한게 순서
 			add(button2);
+			add(button3);
+			add(button4);
+			add(button5);
+			add(button6);
+			add(button7);
+			add(button8);
+			add(button9);
+			add(button0);
+			add(plus);
+			add(c);
+			add(equal);
 
-			setSize(600,200);
+			setSize(300,200);
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			setTitle("Fabulous 3rd frame");
+			//EXIT_ON_CLOSE Static Variable 
+			setTitle("Calculator(+only)");
 			setVisible(true);
+			//프레임은 최상위 컨테이너, 다른 컨테이너 안에 포함될 수 없다.
+			//JFrame JDialog JApplet
+			pack();
 		}
 
 		@Override
@@ -46,14 +97,14 @@
 			// TODO Auto-generated method stub
 			//아래 보면 getActionCommand로 가져오는거 있음
 			if((click.getActionCommand()).equals("button1")) {
-				button.setText("눌린FaButtion");
+				
 				}
-			else if((fable.getActionCommand()).equals("button2")) {
-				button2.setText("눌린Able");
+			if((click.getActionCommand()).equals("button2")) {
+				
 			}
-			a++;
-			System.out.println("버튼이 " + a + "번 눌렸어요");
-			tf.setText("버튼 누르지 말고 위시 입력해줘요 ㅠㅠ");
+			String result;
+			result =this.tostring();
+			numberField.setText(result);
 		}
 	}
 
--- 2017440140 HW3/src/tew/test.java
+++ 2017440140 HW3/src/tew/test.java
@@ -1,9 +1,12 @@
 package tew;
 
+import javax.swing.JFrame;
+
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		JFrame Calframe = new CalcFrame();
 
 	}
 
